49d3262e18674b127ef5998ca0d3816a056ed948
Gobs <sebastian_gonzato@protonmail.com>
2021-12-08 16:41:59 +0100

    Added GEPPR

--- instantiate.jl
+++ instantiate.jl
@@ -3,13 +3,18 @@ juliaenv = "." # @__DIR__ is no good because it gives the absolute paths
 cd(@__DIR__)
 Pkg.activate(juliaenv)
 ENV["JULIA_PKG_DEVDIR"] = joinpath(juliaenv, "dev")
+
+# Add my UC/ED/GEP package, GEPPR
 try
     Pkg.remove("GEPPR")
 catch
     nothing
 end
-Pkg.add(url="https://gitlab.kuleuven.be/UCM/GEPPR.jl.git", rev="dev")
+Pkg.add(url="https://gitlab.kuleuven.be/UCM/GEPPR.jl.git")
 Pkg.develop("GEPPR")
 cd(joinpath(@__DIR__, "dev", "GEPPR"))
-run(`git checkout dev`)
+run(`git checkout a_sos_EPOC`) # If this fails then do this manually
 cd(joinpath("..", ".."))
+
+# Instantiate
+Pkg.instantiate()
