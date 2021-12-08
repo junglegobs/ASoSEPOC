5f7b4605a7a3e92269fdeb11b0d2dfd9acc6a2b4
Gobs <sebastian_gonzato@protonmail.com>
2021-12-08 06:49:59 +0100

    Add RES to PowerModels dict

--- scripts/process_belderbos_data.jl
+++ scripts/process_belderbos_data.jl
@@ -1,7 +1,7 @@
 using DrWatson
 @quickactivate
-using XLSX, PowerModels
-net_data_dir = datadir("exp_raw", "Belderbos_Belgian_Model")
+using XLSX, PowerModels, JSON
+net_data_dir = datadir("raw", "Belderbos_Belgian_Model")
 
 network = Dict{String,Any}(
     "name" => "Belderbos Belgium Model",
@@ -14,7 +14,9 @@ network = Dict{String,Any}(
     "gen" => Dict{String,Any}(),
 )
 grid = XLSX.readxlsx(joinpath(net_data_dir, "BE-full2015_grid.xlsx"))
-load_cols, load_labs = XLSX.readtable(joinpath(net_data_dir, "BE-full2015_load.xlsx"), "load")
+load_cols, load_labs = XLSX.readtable(
+    joinpath(net_data_dir, "BE-full2015_load.xlsx"), "load"
+)
 gen = XLSX.readxlsx(joinpath(net_data_dir, "BE-full2015_portfolio.xlsx"))
 ts = XLSX.readxlsx(joinpath(net_data_dir, "BE-full2015_transactions.xlsx"))
 prices = XLSX.readxlsx(joinpath(net_data_dir, "BE-full2015_prices.xlsx"))
@@ -31,13 +33,13 @@ for row in XLSX.eachrow(node_sh)
             "bus_i" => "$bus_id",
             "source_id" => ["bus", bus_id],
             "zone" => 1,
-            "area" => 1
+            "area" => 1,
             "bus_type" => bus_type,
-            "vmin" => 0.9 # TODO, assumed
-            "vmax" => 1.1 # TODO, assumed
-            "va" => 0.0 # TODO, no idea
-            "vm" => 0.0 # TODO, no idea
-            "base_kv" => 380.0 # TODO, assumed since not given
+            "vmin" => 0.9,
+            "vmax" => 1.1,
+            "va" => 0.0,
+            "vm" => 0.0,
+            "base_kv" => 380.0, # TODO, assumed since not given
         )
     end
 end
@@ -50,7 +52,7 @@ for row in XLSX.eachrow(line_sh)
         network["branch"]["$line_id"] = Dict(
             "name" => "line_$line_id",
             "number_id" => line_id,
-            "index" -> line_id,
+            "index" => line_id,
             "f_bus" => row["C"],
             "t_bus" => row["D"],
             "rate_a" => row["F"], # TODO - this is the line capacity right?
@@ -64,7 +66,7 @@ for row in XLSX.eachrow(line_sh)
 end
 
 # Load
-for i = 1:length(load_cols)
+for i in 1:length(load_cols)
     load_labs[i] == Symbol("Demand  [MW]") && continue
     bus = string(load_labs[i])
     bus_id = parse(Int, split(bus, "_")[end])
@@ -74,7 +76,7 @@ for i = 1:length(load_cols)
         "load_bus" => bus_id,
         "pd" => load_vec,
         "pq" => fill(0.0, length(load_vec)),
-        "index" => 1
+        "index" => 1,
     )
 end
 
@@ -87,31 +89,56 @@ for row in XLSX.eachrow(pp_sh)
     catg = row["C"]
     catg_row = gen_catg_sh["A$catg:X$catg"]
     fuel = catg_row[5]
-    fuel_cost = prices["fuel_prices"][4,fuel+1]
+    fuel_cost = prices["fuel_prices"][4, fuel + 1]
     cost = fuel / catg_row[3] # Euros/MWh
     network["gen"]["$gen_id"] = Dict(
+        "name" => row["B"],
+        "bus" => row["D"],
         "source_id" => ["gen", row["D"]], # second element is bus,
         "index" => gen_id,
         "pmax" => row["E"],
-        "pmin" => row["E"] * row["G"] / 100
+        "pmin" => row["E"] * row["G"] / 100,
         "qmin" => 0.0, # TODO ???
         "qmax" => 0.0, # TODO ???
         "startup" => row["P"], # In kEur per start
         "shutdown" => 0.0,
-        "gen_status" => 1 # i.e. On / availaible
+        "gen_status" => 1,
         "cost" => cost, #  Euros / MWh
-        "ncost" => 1, 
+        "ncost" => 1,
         "min_up" => catg_row[12], # TODO not part of PowerModels format
         "min_down" => catg_row[11], # TODO not part of PowerModels format
-        "qg" => 0.0 # TODO not sure what this is
-        "vg" => 0.0 # TODO not sure what this is
+        "qg" => 0.0,
+        "vg" => 0.0, # TODO not sure what this is
     )
 end
 
-# TODO renewables
+# Renewables
+res_sh_dict = Dict(
+    res_name => ts[res_name] for
+    res_name in ["Sun", "Wind onshore", "Wind offshore"]
+)
+n_nodes = length(network["bus"])
+network["res"] = Dict{String,Any}()
+id = 1
+for (res_name, res_sh) in res_sh_dict
+    for col in (1:n_nodes) .+ 5
+        node = res_sh[1, col]
+        ismissing(node) && continue
+        res_id = string(id)
+        id += 1
+        af = Float64[]
+        for row in XLSX.eachrow(res_sh)
+            row["E"] isa AbstractString && continue
+            push!(af, row[col])
+        end
+        network["res"][res_id] = Dict(
+            "af" => af, "name" => res_id, "bus" => node
+        )
+    end
+end
 
 # Save the dictionary
-exp_pro = datadir("exp_pro")
-open(joinpath(exp_pro, "grid.json"),"w") do f
+exp_pro = datadir("pro")
+open(joinpath(exp_pro, "grid.json"), "w") do f
     JSON.print(f, network, 4)
 end
