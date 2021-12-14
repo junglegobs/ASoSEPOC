6d35d20d817ce817bd0bca628e907e3d59e53e4d
Gobs <sebastian_gonzato@protonmail.com>
2021-12-14 16:54:44 +0100

    UC for 3 days up and running

--- scripts/process_input.jl
+++ scripts/process_input.jl
@@ -18,17 +18,13 @@ opts = options(
     "unit_commitment_type" => "none",
     "operating_reserves_sizing_type" => "given",
     "operating_reserves_type" => "none",
-    "optimization_horizon" => [1, 8_760]
+    "optimization_horizon" => [1, 8_760],
+    "save_path" => datadir("sims", "linear_w_storage"),
 )
 gep = run_GEPPR(opts)
-save_optimisation_values!(gep) # In case not saved due to TimeOut limit
-save(gep, datadir("sims", "linear_w_storage"))
 
 # Identify 3 days to investigate - no load shedding at all, almost load shedding and load shedding
-day_no_scarce, day_some_scarce, day_scarce = days_to_run_models_on(gep)
+days_to_run_models_on(gep, "days_for_analysis.csv")
 
 # Save storage dispatch as a node injection
 storage_dispatch_2_node_injection(gep, grid_path, grid_wo_store_path)
-
-# Re_process data to be used with GEPPR
-powermodels_2_GEPPR(grid_wo_store_path, grid_wo_store_red_path)
