43a69f11a124c1ac928da00f1380f6381de924b5
Gobs <sebastian_gonzato@protonmail.com>
2022-02-16 14:51:08 +0100

    Implement scenario checking

--- scripts/test_scenario_loading.jl
+++ scripts/test_scenario_loading.jl
@@ -1,20 +1,8 @@
 include(joinpath(@__DIR__, "..", "intro.jl"))
 
 files_dict = Dict(
-    "Load" => datadir(
-        "raw",
-        "Forecast_Error_Scenarios",
-        "1000SC_BELDERBOS_load_1_01-20-2022",
-    ),
-    "Solar" => datadir(
-        "raw",
-        "Forecast_Error_Scenarios",
-        "1000SC_BELDERBOS_solar_1_01-20-2022",
-    ),
-    "Wind" => datadir(
-        "raw",
-        "Forecast_Error_Scenarios",
-        "1000SC_BELDERBOS_wind_1_01-20-2022",
-    ),
+    "Load" => scendir("1000SC_BELDERBOS_load_1"),
+    "Solar" => scendir("1000SC_BELDERBOS_solar_1"),
+    "Wind" => scendir("1000SC_BELDERBOS_wind_1"),
 )
 scen_dict = load_scenarios(Dict(), files_dict)
