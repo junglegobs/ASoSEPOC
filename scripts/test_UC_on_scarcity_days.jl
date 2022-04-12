605317e85c08bc372022d3af24d54ca2dbee5eaf
Gobs <sebastian_gonzato@protonmail.com>
2022-04-12 15:01:33 +0200

    Infeasability fixed

--- scripts/test_UC_on_scarcity_days.jl
+++ scripts/test_UC_on_scarcity_days.jl
@@ -2,22 +2,8 @@ include(joinpath(@__DIR__, "..", "intro.jl"))
 sn = script_name(@__FILE__)
 
 df = CSV.read(datadir("pro", "days_for_analysis.csv"), DataFrame)
-opts = options(
-    "include_storage" => false,
-    "include_operating_reserves" => false,
-    "operating_reserves_sizing_type" => "given",
-    "operating_reserves_type" => "none",
-    "initial_commitment_data" => datadir("sims", "rolling_UC_full_year"),
-)
-opts_vec = [
-    merge(
-        opts,
-        Dict(
-            "optimization_horizon" => parse(UnitRange{Int}, df[i,"timesteps"]),
-            "save_path" => datadir("sims", "$(sn)_$(df[i,"days"])"),
-        ),
-    ) for i in 1:size(df, 1)
-]
+opts_vec = options_3_days(sn)
+map(x -> rm(x["save_path"]; force=true, recursive=true), opts_vec)
 gep_vec = run_GEPPR(opts_vec)
 
 # Plot
