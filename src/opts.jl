a6568090ba15bfc8278f568f5c5e8cb5fc2f966f
Gobs <sebastian_gonzato@protonmail.com>
2022-04-25 10:12:01 +0200

    Modify scen load for diff days

--- src/opts.jl
+++ src/opts.jl
@@ -11,7 +11,7 @@ function options(pairs...)
         "operating_reserves_sizing_type" => "given",
         "operating_reserves_type" => "none",
         "initial_commitment_data_path" => "",
-        "optimization_horizon" => [1,48],
+        "optimization_horizon" => [1, 48],
         "rolling_horizon" => false,
         "upward_reserve_levels" => 10,
         "upward_reserve_levels_included_in_redispatch" => [],
@@ -23,37 +23,33 @@ function options(pairs...)
         "vars_2_save" => Symbol[],
         "exprs_2_save" => Symbol[],
     )
-    for (k,v) in pairs
+    for (k, v) in pairs
         opts[k] = v
     end
     return opts
 end
 
 function options_diff_days(sn)
-    error()
     df = CSV.read(datadir("pro", "days_for_analysis.csv"), DataFrame)
     opts = options(
-    "include_storage" => false,
-    "operating_reserves_sizing_type" => "given",
-    "operating_reserves_type" => "none",
-    # "initial_commitment_data_path" => datadir("sims", "rolling_UC_full_year"),
-    "operating_reserves_type" => "probabilistic",
-    "operating_reserves_sizing_type" => "given",
-    "vars_2_save" => [:z, :q],
+        "include_storage" => false,
+        "operating_reserves_sizing_type" => "given",
+        "operating_reserves_type" => "none",
+        # "initial_commitment_data_path" => datadir("sims", "rolling_UC_full_year"),
+        "operating_reserves_type" => "probabilistic",
+        "operating_reserves_sizing_type" => "given",
+        "vars_2_save" => [:z, :q],
     )
-    scen_ids = [7,1,2]
     opts_vec = [
         merge(
             opts,
             Dict(
-                "optimization_horizon" => parse(UnitRange{Int}, df[i, "timesteps"]),
+                "optimization_horizon" =>
+                    parse(UnitRange{Int}, df[i, "timesteps"]),
                 "save_path" => datadir("sims", "$(sn)_$(df[i,"days"])"),
-                "load_scenario_data_paths" =>
-                    scendir.("1000SC_BELDERBOS_load_$(scen_ids[i])",),
-                "solar_scenario_data_paths" =>
-                    scendir.("1000SC_BELDERBOS_solar_$(scen_ids[i])"),
-                "wind_scenario_data_paths" =>
-                    scendir.("1000SC_BELDERBOS_wind_$(scen_ids[i])"),
+                "load_scenario_data_paths" => "1000SC_BELDERBOS_load_$(df[i,"month"])_$(df[i,"day_of_month"])",
+                "solar_scenario_data_paths" => "1000SC_BELDERBOS_solar_$(df[i,"month"])_$(df[i,"day_of_month"])",
+                "wind_scenario_data_paths" => "1000SC_BELDERBOS_wind_$(df[i,"month"])_$(df[i,"day_of_month"])",
             ),
         ) for i in 1:size(df, 1)
     ]
