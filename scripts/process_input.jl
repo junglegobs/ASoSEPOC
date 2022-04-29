f8065cc114de06f4e28154328b6b36640c162c7c
Gobs <sebastian_gonzato@protonmail.com>
2022-04-29 11:56:34 +0200

    Include storage SOC in grid file

--- scripts/process_input.jl
+++ scripts/process_input.jl
@@ -39,13 +39,15 @@ ls = sum(
     );
     dims=1,
 )[:]
-Plots.plot(ls, xlab="Day", ylab="Load shedding [MW]", lab="")
+Plots.plot(ls; xlab="Day", ylab="Load shedding [MW]", lab="")
 Plots.savefig(simsdir(sn, "load_shedding_daily_timeseries.png"))
-Plots.plot(sort(ls; rev=true), xlab="Day", ylab="Load shedding [MW]", lab="")
+Plots.plot(sort(ls; rev=true); xlab="Day", ylab="Load shedding [MW]", lab="")
 Plots.savefig(simsdir(sn, "load_shedding_daily_timeseries_sorted.png"))
 
 # Save storage dispatch as a node injection
-storage_dispatch_2_node_injection(gep, grid_path, grid_wo_store_path)
+storage_dispatch_2_node_injection(
+    gep, grid_path, grid_wo_store_path, grid_w_store_ts_path
+)
 
 # Check that dispatches make sense
 opts["save_path"] = ""
