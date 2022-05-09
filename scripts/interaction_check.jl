4c10baa86931efa3f5d7191a6bf9de0c081ed600
Gobs <sebastian_gonzato@protonmail.com>
2022-05-09 15:41:01 +0200

    Add interaction check script

---
+++ scripts/interaction_check.jl
@@ -0,0 +1,9 @@
+include(joinpath(@__DIR__, "..", "intro.jl"))
+sn = script_name(@__FILE__)
+mkrootdirs(plotsdir(sn))
+mkrootdirs(simsdir(sn))
+
+opts = options_diff_days(sn)[1]
+opts["initial_state_of_charge"] = 0.0
+gep = run_GEPPR(opts)
+d = save_gep_for_security_analysis(gep, opts)
