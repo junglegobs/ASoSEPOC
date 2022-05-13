26edfbbaf176904db9966991e4885005314114d5
Gobs <sebastian_gonzato@protonmail.com>
2022-05-13 13:47:13 +0200

    Boff

--- scripts/DUCPR_RSL_debug.jl
+++ scripts/DUCPR_RSL_debug.jl
@@ -6,11 +6,14 @@ rm(simsdir(sn); force=true, recursive=true)
 opts = options_diff_days(sn)[4]
 rm(opts["save_path"]; force=true, recursive=true)
 # opts["unit_commitment_type"] = "none"
+# opts["copperplate"] = true
+# opts["rate_a_multiplier"] = 100
+opts["load_multiplier"] = 1.5
 opts["initial_state_of_charge"] = 0.0
 opts["absolute_limits_on_nodal_imbalance"] = true
 # opts["prevent_simultaneous_charge_and_discharge"] = false
-opts["upward_reserve_levels_included_in_redispatch"] = 1:10
-opts["downward_reserve_levels_included_in_redispatch"] = 1:10
+opts["upward_reserve_levels_included_in_redispatch"] = [9,10]
+opts["downward_reserve_levels_included_in_redispatch"] = [9,10]
 
 RSL_vec = [1.0, 0.5, 0.0]
 opts_vec = [
@@ -23,12 +26,14 @@ opts_vec = [
     ) for v in RSL_vec
 ]
 
-gep = run_GEPPR(opts_vec[1])
-
 gep_vec = run_GEPPR(opts_vec)
-
 df = DataFrame(
     "Reserve shedding limit" => RSL_vec,
     "Reserve Shedding" => sum.([gep[:rsL⁺] for gep in gep_vec]),
     "Load shedding" => sum.([gep[:loadShedding] for gep in gep_vec]),
 )
+
+for i in 1:length(opts_vec)
+    display(plot_dispatch(gep_vec[i], 1))
+    display(plot_reserves_simple(gep_vec[i], 1))
+end
