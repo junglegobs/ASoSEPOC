046e63fc8a5f70e0a056991fafd0b096e4efd799
Gobs <sebastian_gonzato@protonmail.com>
2022-05-30 13:42:04 +0200

    Fix bug regarding limits on nodal imbalance

--- scripts/nodal_imbalance_limits_investigation.jl
+++ scripts/nodal_imbalance_limits_investigation.jl
@@ -3,23 +3,31 @@ sn = script_name(@__FILE__)
 mkrootdirs(plotsdir(sn))
 rm(simsdir(sn); force=true, recursive=true)
 
-opts = options_diff_days(sn)[3]
+opts = options_diff_days(sn)[4]
+opts["initial_state_of_charge"] = 0.5
+# opts["optimization_horizon"] = (7393, 7394)
+opts["load_multiplier"] = 1.5
 opts["initial_state_of_charge"] = 0.0
-# opts["reserve_shedding_limit"] => 0.05
 opts["unit_commitment_type"] = "none"
+opts["time_out"] = 120
+
+opts["absolute_limit_on_nodal_imbalance"] = false
+gep_no_lim = run_GEPPR(opts)
+
 opts["upward_reserve_levels_included_in_redispatch"] = 1:10
 opts["downward_reserve_levels_included_in_redispatch"] = 1:10
 opts["absolute_limit_on_nodal_imbalance"] = true
-opts["time_out"] = 120
+opts["save_path"] *= "_abs"
 gep_abs_lim = run_GEPPR(opts)
 
-opts["absolute_limit_on_nodal_imbalance"] = false
-opts["save_path"] *= "_n"
-gep_no_lim = run_GEPPR(opts)
+opts["convex_hull_limits_on_nodal_imbalance"] = true
+opts["save_path"] *= "_conv"
+gep_conv_lim = run_GEPPR(opts)
 
-gep_vec = [gep_no_lim, gep_abs_lim]
+gep_vec = [gep_no_lim, gep_abs_lim, gep_conv_lim]
 df = DataFrame(
-    "Limits" => ["Absolute", "None"],
+    "Limits" => ["None", "Absolute", "Convex"],
     "Reserve Shedding" => sum.([gep[:rsL⁺] for gep in gep_vec]),
     "Load shedding" => sum.([gep[:loadShedding] for gep in gep_vec]),
+    "Objective" => [gep[:objective] for gep in gep_vec],
 )
