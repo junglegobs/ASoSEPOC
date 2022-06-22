d638b01aa0d3d34b7025e4512f8c418352b9530d
Sebastian Gonzato <sebastian_gonzato@protonmail.com>
2022-06-22 15:45:17 +0200

    Continue investigation of edge effects

--- scripts/debug_unit_commitment_edge_effects.jl
+++ scripts/debug_unit_commitment_edge_effects.jl
@@ -3,12 +3,30 @@ sn = script_name(@__FILE__)
 mkrootdirs(plotsdir(sn))
 mkrootdirs(simsdir(sn))
 
-opts = options_diff_days(sn)[1]
-opts["initial_state_of_charge"] = 0.0
-opts["operating_reserves_type"] = "none"
-opts["time_out"] = 120
-opts["save_path"] *= "_no_reserves"
-gep = run_GEPPR(opts)
+for T in [[0,8], [0,12], [0, 23]]
+    opts = options_diff_days(sn)[i]
+    append!(opts["vars_2_save"], [:w, :v])
+    TH = opts["optimization_horizon"]
+    opts["optimization_horizon"] = (TH[1] + T[1], TH[1] + T[end])
+    opts["initial_state_of_charge"] = 0.0
+    opts["operating_reserves_type"] = "none"
+    opts["time_out"] = 120
+    opts["save_path"] *= "$(T)_no_reserves"
+    gep_no_res = run_GEPPR(opts)
 
-opts["operating_reserves_type"] = "probabilistic"
-gep = run_GEPPR(opts)
+    opts["save_path"] = replace(opts["save_path"], "_no" => "")
+    opts["operating_reserves_type"] = "probabilistic"
+    gep_res = run_GEPPR(opts)
+
+    GC.gc()
+
+    @info "T = $(opts["optimization_horizon"])"
+    for gep in [gep_no_res, gep_res]
+        @info "Reserves: $(GEPPR.get_reserve_type(gep))"
+        for s in [:z,:w,:v]
+            sv = gep[s]
+            sv_sum = sum(sv.data, dims=(1,2,3))[:]
+            @info "Sum of $(s) for last 4 time steps: $sv_sum"
+        end 
+    end
+end
