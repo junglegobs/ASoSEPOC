853a0794ce1a91a4f9490c5934c4e140cf4a73b0
Sebastian Gonzato <sebastian_gonzato@protonmail.com>
2022-07-01 17:52:31 +0200

    script bug fix (minor)

--- scripts/interaction_selected_days.jl
+++ scripts/interaction_selected_days.jl
@@ -3,26 +3,32 @@ sn = script_name(@__FILE__)
 mkrootdirs.(plotsdir(sn), simsdir(sn))
 
 opts_vec = options_diff_days(sn)
-opts["time_out"] = 1200
-opts["initial_state_of_charge"] = 0.5
-opts["upward_reserve_levels_included_in_redispatch"] = 1:10
-opts["downward_reserve_levels_included_in_redispatch"] = 1:10
+opts_vec = [
+    merge(
+        opts,
+        Dict(
+            "time_out" => 1200,
+            "initial_state_of_charge" => 0.5,
+            "upward_reserve_levels_included_in_redispatch" => 1:10,
+            "downward_reserve_levels_included_in_redispatch" => 1:10,
+        ),
+    ) for opts in opts_vec
+]
 opts_vec = [
     merge(
         opts,
         Dict(
             "absolute_limit_on_nodal_imbalance" => false,
-            "save_path" => opts["save_path"] * "_AbsIm=false"
+            "save_path" => opts["save_path"] * "_AbsIm=false",
         ),
     ),
     merge(
         opts,
         Dict(
             "absolute_limit_on_nodal_imbalance" => true,
-            "save_path" => opts["save_path"] * "_AbsIm=true"
+            "save_path" => opts["save_path"] * "_AbsIm=true",
         ),
-    )
-    for opts in opts_vec
+    ) for opts in opts_vec
 ]
 gep = run_GEPPR(opts_vec)
 d_vec = [save_gep_for_security_analysis(gep, opts) for opts in opts_vec]
