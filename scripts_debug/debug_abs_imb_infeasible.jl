0ca2945880abcf4e9a9cab59e8cbe1cddbbe8626
Sebastian Gonzato <sebastian_gonzato@protonmail.com>
2022-07-01 16:52:07 +0200

    Seems feasible again (???)

--- scripts_debug/debug_abs_imb_infeasible.jl
+++ scripts_debug/debug_abs_imb_infeasible.jl
@@ -13,6 +13,12 @@ opts["absolute_imbalance_slack_penalty"] = 1e2
 rm(opts["save_path"]; force=true, recursive=true)
 gep_UC = run_GEPPR(opts)
 
+opts = options_diff_days(sn, "days_for_analysis_2022_07_01.csv")[4]
+opts["save_path"] *= "_no_slack"
+opts["allow_absolute_imbalance_slacks"] = false
+rm(opts["save_path"]; force=true, recursive=true)
+gep_UC_no_sl = run_GEPPR(opts)
+
 opts["unit_commitment_type"] = "none"
 opts["save_path"] *= "_no_UC"
 rm(opts["save_path"]; force=true, recursive=true)
