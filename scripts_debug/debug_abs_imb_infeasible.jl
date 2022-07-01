568ec43330e7d72276d9b34d5d0f6b8244eccaea
Sebastian Gonzato <sebastian_gonzato@protonmail.com>
2022-07-01 17:30:46 +0200

    Clean up abs imb debug script

--- scripts_debug/debug_abs_imb_infeasible.jl
+++ scripts_debug/debug_abs_imb_infeasible.jl
@@ -2,25 +2,28 @@ include(joinpath(@__DIR__, "..", "intro.jl"))
 sn = script_name(@__FILE__)
 mkrootdirs(plotsdir(sn))
 
-opts = options_diff_days(sn, "days_for_analysis_2022_07_01.csv")[4]
-opts["initial_state_of_charge"] = 0.5
-opts["time_out"] = 600
-opts["upward_reserve_levels_included_in_redispatch"] = [10]
-opts["downward_reserve_levels_included_in_redispatch"] = [10]
-opts["absolute_limit_on_nodal_imbalance"] = true
-opts["allow_absolute_imbalance_slacks"] = true
-opts["absolute_imbalance_slack_penalty"] = 1e2
-rm(opts["save_path"]; force=true, recursive=true)
-gep_UC = run_GEPPR(opts)
+function run_debug_abs_imb_infeasible(L)
+    opts = options_diff_days(sn, "days_for_analysis_2022_07_01.csv")[4]
+    opts["save_path"] *= "_L=$L"
+    opts["initial_state_of_charge"] = 0.5
+    opts["time_out"] = 600
+    opts["upward_reserve_levels_included_in_redispatch"] = L
+    opts["downward_reserve_levels_included_in_redispatch"] = L
+    opts["absolute_limit_on_nodal_imbalance"] = true
+    opts["allow_absolute_imbalance_slacks"] = true
+    opts["absolute_imbalance_slack_penalty"] = 1e2
+    gep_UC = run_GEPPR(opts)
 
-opts = options_diff_days(sn, "days_for_analysis_2022_07_01.csv")[4]
-opts["save_path"] *= "_no_slack"
-opts["allow_absolute_imbalance_slacks"] = false
-rm(opts["save_path"]; force=true, recursive=true)
-gep_UC_no_sl = run_GEPPR(opts)
+    opts = options_diff_days(sn, "days_for_analysis_2022_07_01.csv")[4]
+    opts["save_path"] *= "_no_slack"
+    opts["allow_absolute_imbalance_slacks"] = false
+    gep_UC_no_sl = run_GEPPR(opts)
 
-opts["unit_commitment_type"] = "none"
-opts["save_path"] *= "_no_UC"
-rm(opts["save_path"]; force=true, recursive=true)
-gep_ED = run_GEPPR(opts)
+    opts["unit_commitment_type"] = "none"
+    opts["save_path"] *= "_no_UC"
+    gep_ED = run_GEPPR(opts)
+    return gep_UC, gep_UC_no_sl, gep_ED
+end
 
+gep_UC, gep_UC_no_sl, gep_ED = run_debug_abs_imb_infeasible([10])
+gep_UC, gep_UC_no_sl, gep_ED = run_debug_abs_imb_infeasible(1:10)
